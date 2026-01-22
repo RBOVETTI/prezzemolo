e0f5447d612e7167bbd6aafed36ec33492815284
RBOVETTI <riccardo.bovetti@gmail.com>
2026-01-22 11:51:48 +0100

    casetta

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
             strokeWidth="1.5"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="w-8 h-8 md:w-10 md:h-10"
+            className="w-5 h-5 md:w-6 md:h-6"
           >
             <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
             <polyline points="9 22 9 12 15 12 15 22" />
